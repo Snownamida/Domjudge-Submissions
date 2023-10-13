6fb4e86d03892b5d3b363ae93850332a2ba7d9bc
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-10-13 11:17:48 +0200

    vbs

--- 更新数据.bat
+++ 更新数据.bat
@@ -1,4 +1,6 @@
-
+@echo off
+G:
+cd "G:\WORK\学习\法国\1. INSA\8. IF3\0. Divers\Domjudge"
 curl "http://servif-algo.insa-lyon.fr/domjudge/api/submissions" -o submissions.raw.json
 type submissions.raw.json | json > submissions.json
 curl "http://servif-algo.insa-lyon.fr/domjudge/api/problems" -o problems.raw.json
--- 更新数据.vbs
+++ 更新数据.vbs
@@ -1,4 +1,4 @@
 Dim WinScriptHost
 Set WinScriptHost = CreateObject("WScript.Shell")
-WinScriptHost.Run Chr(34) & "更新数据.bat" & Chr(34), 0
+WinScriptHost.Run Chr(34) & "G:\WORK\学习\法国\1. INSA\8. IF3\0. Divers\Domjudge\更新数据.bat" & Chr(34), 0
 Set WinScriptHost = Nothing
