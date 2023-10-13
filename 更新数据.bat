784a4a05bb8e04f87fc488d2388b619503a0b23c
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-10-13 09:33:11 +0200

    全部自动化!

--- 更新数据.bat
+++ 更新数据.bat
@@ -1,4 +1,6 @@
 
-curl "http://servif-algo.insa-lyon.fr/domjudge/api/submissions" -o submissions.json
-curl "http://servif-algo.insa-lyon.fr/domjudge/api/problems" -o problems.json
+curl "http://servif-algo.insa-lyon.fr/domjudge/api/submissions" -o submissions.raw.json
+type submissions.raw.json | json > submissions.json
+curl "http://servif-algo.insa-lyon.fr/domjudge/api/problems" -o problems.raw.json
+type problems.raw.json | json > problems.json
 
