401c3fd60e8c3f5d6857d4c6fe9fd28b3b9a05d0
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-10-13 09:22:27 +0200

    不能更新team

--- 更新数据.bat
+++ 更新数据.bat
@@ -1,4 +1,4 @@
 
 curl "http://servif-algo.insa-lyon.fr/domjudge/api/submissions" -o submissions.json
 curl "http://servif-algo.insa-lyon.fr/domjudge/api/problems" -o problems.json
-curl "http://servif-algo.insa-lyon.fr/domjudge/api/teams" -o teams.json
+
