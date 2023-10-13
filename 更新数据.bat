5a4026ae032c6dc811c262379418da0f0d23eb71
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-10-13 10:53:02 +0200

    自动上传

--- 更新数据.bat
+++ 更新数据.bat
@@ -3,4 +3,4 @@ curl "http://servif-algo.insa-lyon.fr/domjudge/api/submissions" -o submissions.r
 type submissions.raw.json | json > submissions.json
 curl "http://servif-algo.insa-lyon.fr/domjudge/api/problems" -o problems.raw.json
 type problems.raw.json | json > problems.json
-
+git add submissions.json problems.json && git commit -m "Update data" && git push
